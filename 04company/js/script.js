9f888fdac9e67dd64b9008b98af43e45ed0ed0e0
zeroji08 <youngji6992@gmail.com>
2021-09-15 11:12:23 +0900

    1

--- 04company/js/script.js
+++ 04company/js/script.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-    $(".slider").bxSlider();
+    const slider1 = $(".slider").bxSlider({
+        controls:false
+    });
+    $(".nextBtn").click(function(){
+        slider1.goToNextSlide();
+    });
+    $(".prevBtn").click(function(){
+        slider1.goToPrevSlide();
+    });
+
+    // $(".toggle").on("이벤트명",리스너);
+    // $(".toggle").on("click",function(){});
+    // $(".toggle").click(function(){})
+    $(".toggle").click(function(){
+        const toggle = $(".toggle").attr("class"); // value toggle
+        console.log(toggle);
+
+        if( toggle == "toggle"){
+            //$(".toggle").addClass("active");
+            document.getElementsByClassName("toggle")[0].classList.add("active");
+            $(".gnb").addClass("view");
+        }else{
+            //$(".toggle").removeClass("active");
+            document.getElementsByClassName("toggle")[0].classList.remove("active");
+            $(".gnb").removeClass("view");
+        }
+    });
+
+    $(window).resize(function(){
+        $(".toggle").removeClass("active");
+        $(".gnb").removeClass("view");
+    });
+   
 });
